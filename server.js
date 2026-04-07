9b8919ad82dbefc4d4933794d4e44653bce68605
Alex-B012 <rad15on2014@gmail.com>
2026-04-07 21:30:53 +0300

    cors - improved

--- server.js
+++ server.js
@@ -87,7 +87,10 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Methods",
       "GET,POST,PUT,DELETE,OPTIONS",
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, x-api-key",
+    );
     res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
