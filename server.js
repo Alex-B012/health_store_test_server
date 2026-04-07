b68dabf94602f4ca3edd7137b6d3592267872b0d
Alex-B012 <rad15on2014@gmail.com>
2026-04-07 19:25:15 +0300

    deployment preparation - completed v1.0.8

--- server.js
+++ server.js
@@ -94,14 +94,16 @@ app.use(
 
 // app.use(cors({}));
 
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  }),
-);
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", allowedOrigins.join(","));
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 // --- Middleware ---
 app.use(loggingMiddleware);
