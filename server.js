75b71f8fb4ee05aa0e63a69ea370e5e805070a8b
Alex-B012 <rad15on2014@gmail.com>
2026-04-07 17:59:25 +0300

    deployment preparation - completed v1.0.2

--- server.js
+++ server.js
@@ -67,7 +67,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [SCANNER_URL, MANAGER_URL_APP],
+    origin: [MANAGER_URL_APP, SCANNER_URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
 
